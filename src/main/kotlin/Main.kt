469a84cc54ab7d27cebd4f379bac3b0c6459fa10
Luka23 <lukabella99@icloud.com>
2024-12-27 15:39:08 -0600

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,6 @@ fun main(args: Array<String>) {
     println("Dish soap in the sink...don't blink")
     println("...")
     println("the best i go")
-    println("I eat sourdough")
+    println("I eat sourdough!!!")
     print("$args")
 }
